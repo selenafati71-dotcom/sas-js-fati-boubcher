e7148cf7f84e951d60b8b3c4f3246973066eb0fa
fatimazahra boubcher <selenafati71@gmail.com>
2026-09-08 12:17:56 +0100

    jour 01 : terminer les exercices

--- day01/exercices/exercice-08.js
+++ day01/exercices/exercice-08.js
@@ -14,4 +14,9 @@
 
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
-// TODO: écris ta solution ici.
+
+let texte = "10";
+let nombre = 10;
+
+console.log(texte == nombre);
+console.log(texte === nombre);
