0045932fff11d026426f442a6b8684e41b66e6c7
fatimazahra boubcher <selenafati71@gmail.com>
2026-09-08 15:28:46 +0100

    jour 01 : terminer les exercices

--- day01/exercices/exercice-15.js
+++ day01/exercices/exercice-15.js
@@ -15,6 +15,13 @@
  */
 'use strict';
 
-// 1. Identifie les données nécessaires.
-// 2. Écris ta solution sous cette ligne.
-// TODO: écris ta solution ici.
+
+
+
+let prixExtrait = "  349.99 MAD  ";
+
+let prix = Number(prixExtrait.trim().replace("MAD", "").trim());
+
+let total = prix * 2;
+
+console.log(total);
