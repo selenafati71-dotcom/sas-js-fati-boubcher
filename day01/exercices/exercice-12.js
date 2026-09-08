b525f772d87c29787dc56816b07e80f116d0b857
fatimazahra boubcher <selenafati71@gmail.com>
2026-09-08 14:52:16 +0100

    jour 01 : terminer les exercices

--- day01/exercices/exercice-12.js
+++ day01/exercices/exercice-12.js
@@ -17,4 +17,16 @@
 
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
-// TODO: écris ta solution ici.
+
+
+'use strict';
+
+
+let annee = 2024;
+
+
+if ((annee % 4 === 0 && annee % 100 !== 0) || annee % 400 === 0) {
+    console.log(`${annee} est une année bissextile.`);
+} else {
+    console.log(`${annee} n'est pas une année bissextile.`);
+}
