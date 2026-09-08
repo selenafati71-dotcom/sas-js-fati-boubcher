dadab0a84dd8f4c062a58de8e6f7bd9c71027a12
fatimazahra boubcher <selenafati71@gmail.com>
2026-09-08 11:53:54 +0100

    jour 01 : terminer les exercices

--- day01/exercices/exercice-05.js
+++ day01/exercices/exercice-05.js
@@ -17,4 +17,11 @@
 
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
-// TODO: écris ta solution ici.
+
+let nombre = 15;
+
+if (nombre % 2 === 0) {
+    console.log("Pair");
+} else {
+    console.log("Impair");
+}
