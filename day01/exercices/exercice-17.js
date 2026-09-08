9c7bfc420bebfada03ed2207b7dec666b3ed7654
fatimazahra boubcher <selenafati71@gmail.com>
2026-09-08 15:34:50 +0100

    jour 01 : terminer les exercices

--- day01/exercices/exercice-17.js
+++ day01/exercices/exercice-17.js
@@ -15,6 +15,21 @@
  */
 'use strict';
 
-// 1. Identifie les données nécessaires.
-// 2. Écris ta solution sous cette ligne.
-// TODO: écris ta solution ici.
+
+
+'use strict';
+
+let joueur1 = "Pierre";
+let joueur2 = "Ciseaux";
+
+if (joueur1 === joueur2) {
+    console.log("Égalité !");
+} else if (
+    (joueur1 === "Pierre" && joueur2 === "Ciseaux") ||
+    (joueur1 === "Ciseaux" && joueur2 === "Feuille") ||
+    (joueur1 === "Feuille" && joueur2 === "Pierre")
+) {
+    console.log("Joueur 1 gagne !");
+} else {
+    console.log("Joueur 2 gagne !");
+}
