4dd61956e48e6e7359597c0e44d7658462a01537
fatimazahra boubcher <selenafati71@gmail.com>
2026-09-08 15:01:45 +0100

    jour 01 : terminer les exercices

--- day01/exercices/exercice-13.js
+++ day01/exercices/exercice-13.js
@@ -17,4 +17,33 @@
 
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
-// TODO: écris ta solution ici.
+
+
+'use strict';
+
+
+let nombre1 = 10;
+let nombre2 = 5;
+let operateur = "*";
+
+
+switch (operateur) {
+    case "+":
+        console.log(nombre1 + nombre2);
+        break;
+
+    case "-":
+        console.log(nombre1 - nombre2);
+        break;
+
+    case "*":
+        console.log(nombre1 * nombre2);
+        break;
+
+    case "/":
+        console.log(nombre1 / nombre2);
+        break;
+
+    default:
+        console.log("Opérateur invalide");
+}
