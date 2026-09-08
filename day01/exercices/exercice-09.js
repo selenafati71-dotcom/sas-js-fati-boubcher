cee50c6592194986b38bab9ca26f2c9844f8ff32
fatimazahra boubcher <selenafati71@gmail.com>
2026-09-08 12:23:01 +0100

    jour 01 : terminer les exercices

--- day01/exercices/exercice-09.js
+++ day01/exercices/exercice-09.js
@@ -17,4 +17,14 @@
 
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
-// TODO: écris ta solution ici.
+
+let estAdmin = false;
+let estModerateur = true;
+let motDePasseValide = true;
+
+if (motDePasseValide && (estAdmin || estModerateur)) {
+    console.log("Accès Accordé");
+} else {
+    console.log("Accès Refusé");
+}
+
