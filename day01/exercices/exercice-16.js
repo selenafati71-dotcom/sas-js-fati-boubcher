f922b2b59315bdddd0dff47f6e706112295e6e64
fatimazahra boubcher <selenafati71@gmail.com>
2026-09-08 15:39:20 +0100

    jour 01 : terminer les exercices

--- day01/exercices/exercice-16.js
+++ day01/exercices/exercice-16.js
@@ -12,6 +12,14 @@
  */
 'use strict';
 
-// 1. Identifie les données nécessaires.
-// 2. Écris ta solution sous cette ligne.
-// TODO: écris ta solution ici.
+
+
+
+let numero = "0612345678";
+
+
+if (typeof numero === "string" && numero.length === 10) {
+    console.log("Numéro valide");
+} else {
+    console.log("Numéro invalide");
+}
