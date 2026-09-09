24f8819b98fb92dce56fc71cf6bc6c6486d1387f
fatimazahra boubcher <selenafati71@gmail.com>
2026-09-09 11:45:06 +0100

    jour 02 : terminer les exercices 01 a 07

--- day02/exercices/exercice-02.js
+++ day02/exercices/exercice-02.js
@@ -13,5 +13,11 @@
 'use strict';
 
 // 1. Identifie les données nécessaires.
-// 2. Écris ta solution sous cette ligne.
-// TODO: écris ta solution ici.
+// 2. Écris ta solution sous cette ligne
+
+for (let i = 0; i<=20; i+=2) {
+    console.log(i);
+}
+
+
+
