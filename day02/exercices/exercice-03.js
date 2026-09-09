6fb14de79a5cb3428248684b2aaef8d5ffba2100
fatimazahra boubcher <selenafati71@gmail.com>
2026-09-09 11:53:12 +0100

    jour 02 : terminer les exercices 01 a 07

--- day02/exercices/exercice-03.js
+++ day02/exercices/exercice-03.js
@@ -14,4 +14,8 @@
 
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
-// TODO: écris ta solution ici.
+
+
+for (let i = 1; i<=10; i++) {
+    console.log(`${i} x 7 = ${i * 7}`);
+}
