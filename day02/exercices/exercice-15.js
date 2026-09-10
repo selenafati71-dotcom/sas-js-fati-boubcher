42790da080a5c7ee96f2030c5ca4a6e33125e753
fatimazahra boubcher <selenafati71@gmail.com>
2026-09-10 12:18:32 +0100

    jour 02 : terminer les exercices 01 a 07

--- day02/exercices/exercice-15.js
+++ day02/exercices/exercice-15.js
@@ -16,4 +16,11 @@
 
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
-// TODO: écris ta solution ici.
+
+for (let i = 1 ; i <=5; i++) {
+   let ligne = "";
+    for (let j = 1; j <=i; j++) {
+        ligne +=   "*";
+    }
+    console.log(ligne);
+}
