3f90ddd680473bc07b6e09d5e8f42d8c5f2178f3
fatimazahra boubcher <selenafati71@gmail.com>
2026-09-10 15:45:37 +0100

    jour 02 : terminer les exercices 01 a 07

--- day02/challenge/challenge.js
+++ day02/challenge/challenge.js
@@ -21,4 +21,24 @@
 'use strict';
 
 // Découpe d'abord le problème en petites étapes.
-// TODO: écris ta solution ici.
+
+
+
+let codeSecret = 739;
+let essais = 0;
+
+trouver:
+for (let c1 = 0; c1 <= 9; c1++) {
+    for (let c2 = 0; c2 <= 9; c2++) {
+        for (let c3 = 0; c3 <= 9; c3++) {
+
+            let codeTeste = c1 * 100 + c2 * 10 + c3;
+            essais++;
+
+            if (codeTeste === codeSecret) {
+                console.log(`Code trouvé : ${codeSecret} après ${essais} essais`);
+                break trouver;
+            }
+        }
+    }
+}
