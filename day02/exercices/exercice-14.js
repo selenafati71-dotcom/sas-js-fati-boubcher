017ad3a981d1b93947eb6dab3821409d0680b636
fatimazahra boubcher <selenafati71@gmail.com>
2026-09-10 11:53:54 +0100

    jour 02 : terminer les exercices 01 a 07

--- day02/exercices/exercice-14.js
+++ day02/exercices/exercice-14.js
@@ -14,4 +14,10 @@
 
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
-// TODO: écris ta solution ici.
+
+let i = 1;
+while (i * i <= 1000) {
+    i++;
+
+}
+console.log(i * i);
