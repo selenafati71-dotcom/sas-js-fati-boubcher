c1926f6e745adf899abb6b7ee224fe31a5a1577f
fatimazahra boubcher <selenafati71@gmail.com>
2026-09-10 11:13:25 +0100

    jour 02 : terminer les exercices 01 a 07

--- day02/exercices/exercice-10.js
+++ day02/exercices/exercice-10.js
@@ -17,4 +17,13 @@
 
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
-// TODO: écris ta solution ici.
+
+
+
+let resultat = 1;
+
+for (let i = 6; i >= 1; i--) {
+    resultat = resultat * i;
+}
+
+console.log(resultat);
