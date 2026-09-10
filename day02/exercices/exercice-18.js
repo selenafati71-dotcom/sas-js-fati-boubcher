ac7906ffc71e4fad201115b5a0d50cb486617cf5
fatimazahra boubcher <selenafati71@gmail.com>
2026-09-10 14:44:51 +0100

    jour 02 : terminer les exercices 01 a 07

--- day02/exercices/exercice-18.js
+++ day02/exercices/exercice-18.js
@@ -17,4 +17,20 @@
 
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
-// TODO: écris ta solution ici.
+
+
+let N = 15;
+let etapes = 0;
+
+while (N !== 1) {
+
+    if (N % 2 === 0) {
+        N = N / 2;
+    } else {
+        N = N * 3 + 1;
+    }
+
+    etapes++;
+}
+
+console.log("Nombre d'étapes :", etapes);
