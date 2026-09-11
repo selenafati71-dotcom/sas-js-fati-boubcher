64a7136e105f09f0817aecf28de495ede8fd57f5
fatimazahra boubcher <selenafati71@gmail.com>
2026-09-11 10:14:02 +0100

    jour 04 : terminer les exercices 01 a 07

--- day04/exercices/exercice-04.js
+++ day04/exercices/exercice-04.js
@@ -14,4 +14,7 @@
 
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
-// TODO: écris ta solution ici.
+
+const estMajeur =  age => age <= 18
+console.log(estMajeur(20));
+console.log(estMajeur(15));
