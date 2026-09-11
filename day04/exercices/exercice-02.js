6a12b645d4e621b63a781d11a76c475233343cca
fatimazahra boubcher <selenafati71@gmail.com>
2026-09-11 09:21:52 +0100

    jour 04 : terminer les exercices 01 a 07

--- day04/exercices/exercice-02.js
+++ day04/exercices/exercice-02.js
@@ -14,4 +14,6 @@
 
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
-// TODO: écris ta solution ici.
+
+const multiplier = (a, b) => a *b 
+console.log(multiplier(5, 3));
