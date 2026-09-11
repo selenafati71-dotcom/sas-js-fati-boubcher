460aca60c9ed6a992e2e91466db4d75ce894678e
fatimazahra boubcher <selenafati71@gmail.com>
2026-09-11 10:47:05 +0100

    jour 04 : terminer les exercices 01 a 07

--- day04/exercices/exercice-05.js
+++ day04/exercices/exercice-05.js
@@ -14,4 +14,10 @@
 
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
-// TODO: écris ta solution ici.
+
+
+function perimetreRectangle(longueur, largeur = 5) {
+    return 2 * (longueur + largeur);
+}
+
+console.log(perimetreRectangle(10));
