845ac211d84702ab89e195ed8ea744809687bbf5
fatimazahra boubcher <selenafati71@gmail.com>
2026-09-11 19:51:17 +0100

    jour 04 : terminer les exercices 01 a 07

--- day04/exercices/exercice-16.js
+++ day04/exercices/exercice-16.js
@@ -14,4 +14,19 @@
 
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
-// TODO: écris ta solution ici.
+
+
+function creerCompteur() {
+    let count = 0;
+
+    return function() {
+        count++;
+        return count;
+    };
+}
+
+const monCompteur = creerCompteur();
+
+console.log(monCompteur()); 
+console.log(monCompteur()); 
+console.log(monCompteur()); 
