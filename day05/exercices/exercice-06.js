eafe107060c8d1790e64b6ff553aad11e3d129fe
faty_selenna <selenafati71@gmail.com>
2026-09-13 13:54:12 +0100

    jour 05 : terminer les exercices 01 a 07

--- day05/exercices/exercice-06.js
+++ day05/exercices/exercice-06.js
@@ -14,4 +14,7 @@
 
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
-// TODO: écris ta solution ici.
+
+let villes = "Safi,Youssoufia,Nador,Casablanca";
+let tableau = villes.split(",");
+console.log(tableau);
--- day05/exercices/exercice-07.js
+++ day05/exercices/exercice-07.js
@@ -14,4 +14,6 @@
 
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
-// TODO: écris ta solution ici.
+
+let email = "    utilisateur@email.com     ";
+console.log(email.trim());
