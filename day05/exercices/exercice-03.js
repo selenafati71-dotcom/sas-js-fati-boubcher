65221163e6ae5921bc18c079cc539aad03f290d9
faty_selenna <selenafati71@gmail.com>
2026-09-13 13:19:18 +0100

    jour 05 : terminer les exercices 01 a 07

--- day05/exercices/exercice-03.js
+++ day05/exercices/exercice-03.js
@@ -14,4 +14,7 @@
 
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
-// TODO: écris ta solution ici.
+const majuscules = "youcode"
+const minuscules = "MAROC"
+console.log(majuscules.toUpperCase())
+console.log(minuscules.toLocaleLowerCase())
