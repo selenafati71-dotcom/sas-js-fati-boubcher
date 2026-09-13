35ab55bca702e13cd289950f49a61ffb313b02f2
faty_selenna <selenafati71@gmail.com>
2026-09-13 15:37:40 +0100

    jour 05 : terminer les exercices 01 a 07

--- day05/exercices/exercice-12.js
+++ day05/exercices/exercice-12.js
@@ -14,4 +14,7 @@
 
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
-// TODO: écris ta solution ici.
+let phrase = "Bonjour le monde";
+let mots = phrase.split(" ");
+let phraseinverse = mots.reverse().join(" ");
+ console.log(phraseinverse);
