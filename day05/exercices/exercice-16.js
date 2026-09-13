52308b7ea3dfcb0c0b1c76b94474e5354ad59040
faty_selenna <selenafati71@gmail.com>
2026-09-13 16:15:20 +0100

    jour 05 : terminer les exercices 01 a 07

--- day05/exercices/exercice-16.js
+++ day05/exercices/exercice-16.js
@@ -14,4 +14,7 @@
 
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
-// TODO: écris ta solution ici.
+
+let tweet = "Beau temps à #Nador aujourd'hui avec la team #YouCode !";
+let hashtags = tweet.match(/#[a-zA-Z0-9_]+/g);
+console.log(hashtags);
