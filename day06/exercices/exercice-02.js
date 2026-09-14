bf07d1d9e103b31e79f2cef178edef18c552acc1
fatimazahra boubcher <selenafati71@gmail.com>
2026-09-14 15:30:57 +0100

    jour 06 : terminer les exercices 01 a 07

--- day06/exercices/exercice-02.js
+++ day06/exercices/exercice-02.js
@@ -14,4 +14,9 @@
 
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
-// TODO: écris ta solution ici.
+let invites = [];
+const noms = ["yassine", "sara", "amine"];
+invites.push(noms[0]);
+invites.push(noms[1]);
+invites.push(noms[2]);
+console.log(invites);
