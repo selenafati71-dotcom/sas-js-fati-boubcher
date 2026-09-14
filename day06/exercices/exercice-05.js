29fa73feaf0a1061cc9b92315c7097f85f0159fc
fatimazahra boubcher <selenafati71@gmail.com>
2026-09-14 19:00:46 +0100

    jour 06 : terminer les exercices 01 a 07

--- day06/exercices/exercice-05.js
+++ day06/exercices/exercice-05.js
@@ -14,4 +14,6 @@
 
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
-// TODO: écris ta solution ici.
+let ville = ["Rabat", "Casablanca", "Tanger"];
+console.log(ville.includes("Marrakech"));
+
--- day06/exercices/exercice-06.js
+++ day06/exercices/exercice-06.js
@@ -14,4 +14,7 @@
 
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
-// TODO: écris ta solution ici.
+let groupe1 = ["A", "B"];
+let groupe2 = ["C", "D"];
+let resultat = groupe1.concat(groupe2);
+console.log(resultat);
