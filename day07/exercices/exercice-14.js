e9f28b8cca414204e4b94bde21059beed42a1ec3
fatimazahra boubcher <selenafati71@gmail.com>
2026-09-16 17:29:10 +0100

    jour 07 : terminer les exercices 01 a 07

--- day07/exercices/exercice-14.js
+++ day07/exercices/exercice-14.js
@@ -14,4 +14,12 @@
 
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
-// TODO: écris ta solution ici.
+
+let etudiant = {
+    prenom: "Sara",
+    ville: "Rabat",
+     note: 16
+}
+let { prenom, ville } = etudiant ;
+console.log(prenom);
+console.log(ville);
